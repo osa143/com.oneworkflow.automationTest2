3c570fa0cdbd91d5801f99921e3ab51473e99f9c
Mahesh Vaddegani <mahesh.vaddegani@agilenetsystems.com>
2019-09-06 15:57:56 +0100

    Plaza new steps

--- src/test/java/pageObjects/plaza/Plaza_HomePage.java
+++ src/test/java/pageObjects/plaza/Plaza_HomePage.java
@@ -1,8 +1,39 @@
 package pageObjects.plaza;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import pageObjects.BasePage;
+import utils.CommonUtils;
 
 public class Plaza_HomePage extends BasePage {
+
+    public static final String dd_ROLE= "select2-choice";
+    public static final String txt_REQUEST= "form-control ng-pristine ng-valid ng-scope ng-empty ng-valid-maxlength ng-touched";
+    public static final String dd_SYSTEM_ID= "s2id_sp_formfield_sr2_v_system";
+    public static final String txt_PLANNED_START= "sp_formfield_sr2_v_start_date";
+    public static final String txt_PLANNED_END= "sp_formfield_sr2_v_start_date";
+    public static final String txt_DESCRIPTION= "sp_formfield_sr2_v_description";
+    public static final String txt_ADDITIONAL_COMMENTS= "sp_formfield_additional_comments";
+    public static final String btn_ORDER= "x016e0fdbdb854700b27fdb11ce9619e3";
+
+    public void clickOrder(){
+        clickElement(By.id(btn_ORDER));
+    }
+
+    public void enterAdditionalComments(String text){
+        enterTextByElement(By.id(txt_ADDITIONAL_COMMENTS), text);
+    }
+
+    public void enterDescription(String text){
+        enterTextByElement(By.id(txt_DESCRIPTION), text);
+    }
+
+    public void enterPlannedStart(String text){
+        enterTextByElement(By.id(txt_PLANNED_START), text);
+    }
+    public void enterPlannedEnd(String text){
+        enterTextByElement(By.id(txt_PLANNED_END), text);
+    }
 
     public String getTitle_plazaHomePage(){
         String title= driver.getTitle();
@@ -10,4 +41,21 @@ public class Plaza_HomePage extends BasePage {
         return title;
 
     }
+    public void selectDropdown(By Element, String dropdownName){
+        Select role = new Select(findElement(Element));
+        role.selectByVisibleText(dropdownName);
+    }
+    public void selectRoleDropdown(String dropdownName){
+        selectDropdown(By.className(dd_ROLE), dropdownName);
+
+    }
+    public void selectSystemDropdown(String dropdownName)
+    {
+        selectDropdown(By.id(dd_SYSTEM_ID), dropdownName);
+    }
+    public void enterRequest(String text){
+        enterTextByElement(By.className(txt_REQUEST), text);
+    }
+
+
 }
--- src/test/java/steps/plaza_steps/Plaza_HomePageSteps.java
+++ src/test/java/steps/plaza_steps/Plaza_HomePageSteps.java
@@ -1,10 +1,13 @@
 package steps.plaza_steps;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import pageObjects.BasePage;
 import pageObjects.plaza.Plaza_HomePage;
+import utils.CommonUtils;
 
 public class Plaza_HomePageSteps extends BasePage {
     Plaza_HomePage homePage= new Plaza_HomePage();
@@ -13,8 +16,58 @@ public class Plaza_HomePageSteps extends BasePage {
         Assert.assertEquals(homePage.getTitle_plazaHomePage(), "Home - Plaza");
     }
 
-    @When("user clicks on {string} button")
+    @When("user clicks on {string}")
     public void userClicksOnButton(String text) {
         homePage.clickElementByContainsTextAndTagName("*", text);
+    }
+
+    @And("user clicks on IT Infrastructure dropdown")
+    public void userClicksOnITInfrastructureDropdown() {
+        clickElement(By.className("pull-right glyphicon ng-scope glyphicon-chevron-down"));
+    }
+
+    @Then("user should see Finance & Assurance form")
+    public void userShouldSeeFinanceAssuranceForm() {
+
+    }
+
+    @When("user selects role dropdown as {string}")
+    public void userSelectsRoleDropdownAs(String arg0) {
+      homePage.selectRoleDropdown(arg0);
+    }
+
+    @And("user enters {string} in the request field")
+    public void userEntersInTheRequestField(String arg0) {
+        homePage.enterRequest(arg0);
+    }
+
+    @And("user selects system dropdown as {string}")
+    public void userSelectsSystemDropdownAs(String arg0) {
+        homePage.selectSystemDropdown(arg0);
+    }
+
+    @And("user enters planned start time as current time")
+    public void userEntersPlannedStartTimeAsCurrentTime() {
+        homePage.enterPlannedStart(CommonUtils.getDateTime("yyyy-MM-dd HH:mm:ss", "Europe/Stockholm", 0));
+    }
+
+    @And("user enters planned end time as current time")
+    public void userEntersPlannedEndTimeAsCurrentTime() {
+        homePage.enterPlannedEnd(CommonUtils.getDateTime("yyyy-MM-dd HH:mm:ss", "Europe/Stockholm", 0));
+    }
+
+    @And("user enters plaza description as {string}")
+    public void userEntersPlazaDescriptionAs(String arg0) {
+        homePage.enterDescription(arg0);
+    }
+
+    @And("user enters additional comments as {string}")
+    public void userEntersAdditionalCommentsAs(String arg0) {
+        homePage.enterAdditionalComments(arg0);
+    }
+
+    @And("user clicks on order")
+    public void userClicksOnOrder() {
+        homePage.clickOrder();
     }
 }

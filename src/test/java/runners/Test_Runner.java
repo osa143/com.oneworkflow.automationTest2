6ada642e116a92baa957e8dbf5af025ef31dab18
HarveySmith <harvey.smith@agilenetsystems.com>
2020-01-08 13:18:26 +0000

    Updates to SAO-499

--- src/test/java/runners/Test_Runner.java
+++ src/test/java/runners/Test_Runner.java
@@ -7,7 +7,7 @@ import runners.BaseTest;
 @CucumberOptions(features = "classpath:features/",
         glue = {"steps", "cucumberHooks"},
         //dryRun = true,
-        tags = {"@03.04_Link_child_tickets_and_make_master_ticket"}
+        tags = {"@499"}
 )
 public class Test_Runner extends BaseTest {
 }

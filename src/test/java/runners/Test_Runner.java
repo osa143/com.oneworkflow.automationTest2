13459f645bb327af7e429bdbe6909c709349787d
Mayowa <mayowa.osewa@agilenetsystems>
2020-01-08 13:02:17 +0000

    Bulk CIs data updated

--- src/test/java/runners/Test_Runner.java
+++ src/test/java/runners/Test_Runner.java
@@ -7,7 +7,7 @@ import runners.BaseTest;
 @CucumberOptions(features = "classpath:features/",
         glue = {"steps", "cucumberHooks"},
         //dryRun = true,
-        tags = {"@Bulk_Loading_Format_Change_Manual_500CI's_1"}
+        tags = {"@Manual_500CI"}
 )
 public class Test_Runner extends BaseTest {
 }

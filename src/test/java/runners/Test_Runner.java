8cf7cfed2a1f15be43003c8925c6db5a5d706c00
Mayowa <mayowa.osewa@agilenetsystems>
2020-01-16 11:37:58 +0000

    Title Field Structures Validation

--- src/test/java/runners/Test_Runner.java
+++ src/test/java/runners/Test_Runner.java
@@ -7,7 +7,7 @@ import runners.BaseTest;
 @CucumberOptions(features = "classpath:features/",
         glue = {"steps", "cucumberHooks"},
         //dryRun = true,
-        tags = {"@impact_duration_Field_Validation"}
+        tags = {"@Drop_Down"}
 )
 public class Test_Runner extends BaseTest {
 }

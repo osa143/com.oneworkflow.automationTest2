df5f4039673424f92a4490d9999a2d712569cfd4
Mahesh Vaddegani <mahesh.vaddegani@agilenetsystems.com>
2020-01-28 13:13:34 +0000

    Merge remote-tracking branch 'remotes/origin/master' into Mahesh_New
    # Conflicts:
    #	ExtentReports/LoggerReport/dashboard.html
    #	ExtentReports/LoggerReport/exception.html
    #	ExtentReports/LoggerReport/index.html
    #	ExtentReports/LoggerReport/tag.html
    #	build/classes/java/test/pageObjects/BasePage.class
    #	src/test/java/runners/Test_Runner.java

--- src/test/java/runners/Test_Runner.java
+++ src/test/java/runners/Test_Runner.java
@@ -7,7 +7,11 @@ import runners.BaseTest;
 @CucumberOptions(features = "classpath:features/",
         glue = {"steps", "cucumberHooks"},
         //dryRun = true,
-        tags = {"@When_creating_workorder_a_proposal_of_default_technical_escalation_queue_should_appear"}
+
+        tags = {"@"}
+
 )
 public class Test_Runner extends BaseTest {
 }
+
+

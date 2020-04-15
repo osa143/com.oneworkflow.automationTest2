28c08bb4109761ffba36e4f287f0bbc4e8bd5ba7
HarveySmith <harvey.smith@agilenetsystems.com>
2020-04-15 14:34:54 +0100

    Merge remote-tracking branch 'remotes/origin/master' into HavreyBranchNew
    # Conflicts:
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	ExtentReports/LoggerReport/dashboard.html
    #	ExtentReports/LoggerReport/exception.html
    #	ExtentReports/LoggerReport/index.html
    #	ExtentReports/LoggerReport/tag.html
    #	src/test/java/runners/Test_Runner.java
    #	src/test/resources/configs/runtime_test.properties

--- src/test/java/cucumberHooks/Hooks.java
+++ src/test/java/cucumberHooks/Hooks.java
@@ -16,19 +16,19 @@ public class Hooks {
     public void Teardown(Scenario scenario) throws IOException, InterruptedException {
 
         if (scenario.isFailed()) {
-            //byte[] screenshot = BasePage.takeScreenShotAsByteArray();
+//            byte[] screenshot = BasePage.takeScreenShotAsByteArray();
 //        File file = ReporterUtils.saveScreenShot(BasePage.takeScreenShot());
 //        String relativePath = "." + "/" + "Screenshots" + "/" + file.getName();
 //        Path p = Paths.get("Screenshots\\" + file.getName());
-           // scenario.embed(screenshot, "image/png");
+//            scenario.embed(screenshot, "image/png");
         }
 
 
-            OWF_AgentConsolePage agentConsolePage = new OWF_AgentConsolePage();
-            CommonUtils.switchToChildWindow(agentConsolePage.getDriver(), 0);
-            agentConsolePage.clickNavUserMenu();
-            agentConsolePage.clickMenuItemLogout();
-              DriverFactory.getInstance().quit();
+//            OWF_AgentConsolePage agentConsolePage = new OWF_AgentConsolePage();
+//            CommonUtils.switchToChildWindow(agentConsolePage.getDriver(), 0);
+//            agentConsolePage.clickNavUserMenu();
+//            agentConsolePage.clickMenuItemLogout();
+//              DriverFactory.getInstance().quit();
 
        }
     }

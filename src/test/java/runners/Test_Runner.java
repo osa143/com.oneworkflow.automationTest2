55f74be0fd9c9d23647df71f537d01ada2f977ca
HarveySmith <harvey.smith@agilenetsystems.com>
2020-05-27 12:58:42 +0100

    Updating of test cases for P2S2, as well as API

--- src/test/java/runners/Test_Runner.java
+++ src/test/java/runners/Test_Runner.java
@@ -7,7 +7,7 @@ import runners.BaseTest;
 @CucumberOptions(features = "classpath:features/",
         glue = {"steps", "cucumberHooks"},
         //dryRun = true,
-        tags = {"@Multiple_root_causes_code_menus"}
+        tags = {"@Login"}
 
 )
 public class Test_Runner extends BaseTest {

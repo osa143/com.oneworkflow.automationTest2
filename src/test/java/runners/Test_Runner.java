506c0db0df2067935e809b7dd160505553b652c1
Mahesh Vaddegani <mahesh.vaddegani@agilenetsystems.com>
2020-07-01 15:17:26 +0100

    Merge remote-tracking branch 'remotes/origin/Develop' into Mahesh_V
    # Conflicts:
    #	src/test/resources/features/Automation/Regression/Incident/Incident_Functionality/Default_technical_escalation_queue_Appears.feature

--- src/test/java/runners/Test_Runner.java
+++ src/test/java/runners/Test_Runner.java
@@ -7,7 +7,7 @@ import runners.BaseTest;
 @CucumberOptions(features = "classpath:features/",
         plugin = {
                 "pretty",
-                "html:test-output/html",},
+                "html:test-output/html",}, // for html report
         glue = {"steps", "cucumberHooks"},
         //dryRun = true,
         tags = {"@E2E_Security_Console"}
